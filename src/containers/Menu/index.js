d7045b1c4f0a2e6ba063d7bb76a4999892d7d3ea
albaneste <albane.stephant@gmail.com>
2024-09-16 10:06:41 +0200

    commit

---
+++ src/containers/Menu/index.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-return-assign */
+import Button from "../../components/Button";
+import Logo from "../../components/Logo";
+
+import "./style.scss";
+
+const Menu = () => (
+  <nav>
+    <Logo />
+    <ul>
+      <li >
+        <a href="#services">Nos services</a>
+      </li>
+      <li>
+        <a href="#realisations">Nos réalisations</a>
+      </li>
+      <li>
+        <a href="#equipe">Notre équipe</a>
+      </li>
+    </ul>
+    <Button title="contact" onClick={() => (window.document.location.hash = "#contact")}>
+      Contact
+    </Button>
+  </nav>
+);
+
+export default Menu;
